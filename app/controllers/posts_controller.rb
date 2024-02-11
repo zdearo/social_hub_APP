b2481366213de0d18223bc0435a843e590ab16e7
Adryel Dearo <adryelzd77@outlook.com>
2024-02-11 00:47:16 -0300

    Adiciona ação de edição e melhora a exibição dos posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,18 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
